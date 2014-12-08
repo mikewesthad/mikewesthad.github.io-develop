3ff9fd7a004d5c873d78f6484b683aa616bd8ae1
Michael Hadley <michaelwesthadley@gmail.com>
2014-12-08 08:12:49 -0600

    Added thumbnail sized images

---
+++ createThumbnailImages.py
@@ -0,0 +1,24 @@
+import os
+import Image
+
+qualityVal = 95
+desiredWidth = 475.0
+imagesDir = os.path.join(os.getcwd(), "assets", "images")
+
+# Recursively find all files in imagesDir
+for (root, dirs, files) in os.walk(imagesDir):
+	for name in files:
+		base, ext = name.split(".")
+		ext = ext.lower()
+		if ext in ("png", "jpg"):
+			pathToOriginal = os.path.join(root, name)
+			pathToThumbnail = os.path.join(root, base+"Thumbnail.jpg")
+			
+			im = Image.open(pathToOriginal)
+			w, h = im.size
+			if w > desiredWidth:
+				newW = int(desiredWidth)
+				newH = int(desiredWidth/w * h)
+				imThumb = im.resize((newW, newH), Image.ANTIALIAS)
+				imThumb = imThumb.convert("RGB") 
+				imThumb.save(pathToThumbnail, quality=qualityVal)
