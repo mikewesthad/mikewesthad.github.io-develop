56160b26ba8b4eb395a872cc853be4ec7a28e8c9
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-27 14:37:15 -0500

    Nav now triggers loading of any page-specific JS when new content is loaded

--- src/js/main-nav.js
+++ src/js/main-nav.js
@@ -21,12 +21,39 @@ function onNavClick(e) {
     if ($activeNavItem.length) $activeNavItem.removeClass("active");
     $target.addClass("active");
     $activeNavItem = $target;
+    var pageName = $target.data("page-name");
+    loadPage(pageName);    
+}
 
-    var htmlFragmentUrl = $target.data("url");
-    $content.empty();
-    $content.load(htmlFragmentUrl, function (responseText, textStatus, jqXhr) {
+function loadPage(pageName) {
+    var url = "html-pages/" + pageName + ".html";
+    var fadeDuration = 200;
+
+    // Fade then empty the current contents
+    $content.fadeOut(fadeDuration, function () {
+       $content.empty();
+       $content.load(url, onContentFetched);
+    });
+
+    // Fade the new content in after it has been fetched
+    function onContentFetched(responseText, textStatus, jqXhr) {
         if (textStatus === "error") {
             alert("There was a problem loading the page.");
+            return;
         }
-    });
+
+        $content.fadeIn(fadeDuration);
+
+        // If the page needs any specific JS functionality, load it
+        if (pageName === "work") {                
+            require("./scrub-slideshow.js")();
+            require("./hover-overlay.js")();
+
+            var hoverSlideshow = require("./hover-slideshow.js");
+            hoverSlideshow.init(2000, 1000);
+
+            var portfolioFilter = require("./portfolio-filter.js");
+            portfolioFilter.init();
+        }
+    }
 }
