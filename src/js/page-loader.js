af3760efd9c0601dd5724360c31101194ad16a14
Michael Hadley <michaelwesthadley@gmail.com>
2016-06-09 12:59:57 -0500

    Add a module for handling AJAX loading content

---
+++ src/js/page-loader.js
@@ -0,0 +1,42 @@
+module.exports = Loader;
+
+var utilities = require("./utilities.js");
+
+function Loader(onReload, fadeDuration) {
+    this._$content = $("#content");
+    this._onReload = onReload;
+    this._fadeDuration = (fadeDuration !== undefined) ? fadeDuration : 200;
+    this._path = location.pathname;
+}
+
+Loader.prototype.getLoadedPath = function () {
+    return this._path;
+};
+
+Loader.prototype.loadPage = function (url, queryObject, shouldPushHistory) {
+    // Fade then empty the current contents
+    this._$content.fadeOut(this._fadeDuration, function () {
+       this._$content.empty();
+       this._$content.load(url + " #content", onContentFetched.bind(this));
+    }.bind(this));
+
+    // Fade the new content in after it has been fetched
+    function onContentFetched(responseText, textStatus, jqXhr) {
+        if (textStatus === "error") {
+            console.log("There was a problem loading the page.");
+            return;
+        }
+
+        var queryString = utilities.createQueryString(queryObject);
+        if (shouldPushHistory) {
+            history.pushState({
+                url: url,
+                query: queryObject
+            }, null, url + queryString);
+        }
+
+        this._path = location.pathname;
+        this._$content.fadeIn(this._fadeDuration);
+        this._onReload();
+    }
+};
