8bfc7efbe5900c9f5a00533487a141bddca027e5
Michael Hadley <michaelwesthadley@gmail.com>
2016-06-09 12:59:09 -0500

    Make portfolio filter responsive using breakpoints

--- src/js/portfolio-filter.js
+++ src/js/portfolio-filter.js
@@ -1,85 +1,75 @@
-module.exports = {
-    init: init
+module.exports = PortfolioFilter;
+
+var utilities = require("./utilities.js");
+
+var defaultBreakpoints = [
+    { width: 1200, cols: 3, spacing: 15 },
+    { width: 992, cols: 3, spacing: 15 },
+    { width: 700, cols: 3, spacing: 15 },
+    { width: 600, cols: 2, spacing: 10 },
+    { width: 480, cols: 2, spacing: 10 },
+    { width: 320, cols: 1, spacing: 10 }
+];
+
+function PortfolioFilter(loader, breakpoints, aspectRatio, transitionDuration) {
+    this._loader = loader;
+    this._gridSpacing = 0;
+    this._aspectRatio = (aspectRatio !== undefined) ? aspectRatio : (16/9);
+    this._transitionDuration = (transitionDuration !== undefined) ? 
+        transitionDuration : 800;
+    this._breakpoints = (breakpoints !== undefined) ? 
+        breakpoints.slice() : defaultBreakpoints.slice();
+    this._$grid = $("#portfolio-grid");
+    this._$nav = $("#portfolio-nav");
+    this._$projects = [];
+    this._$categories = {};
+    this._rows = 0;
+    this._cols = 0;
+    this._imageHeight = 0;
+    this._imageWidth = 0;
+
+    // Sort the breakpoints in descending order
+    this._breakpoints.sort(function(a, b) {
+        if (a.width < b.width) return -1;
+        else if (a.width > b.width) return 1;
+        else return 0;
+    });
+
+    this._cacheProjects();
+    this._createGrid();
+
+    this._$grid.find(".project a").on("click", this._onProjectClick.bind(this));
+
+    var qs = utilities.getQueryParameters();
+    var initialCategory = qs.category || "all";
+    var category = initialCategory.toLowerCase();
+    this._$activeNavItem = this._$nav.find("a[data-category=" + category + "]");
+    this._$activeNavItem.addClass("active");
+    this._filterProjects(category);
+    $("#portfolio-nav a").on("click", this._onNavClick.bind(this));
+
+    $(window).on("resize", this._createGrid.bind(this));
+}
+
+PortfolioFilter.prototype.selectCategory = function (category) {
+    category = (category && category.toLowerCase()) || "all";
+    var $selectedNav = this._$nav.find("a[data-category=" + category + "]");
+    if ($selectedNav.length && !$selectedNav.is(this._$activeNavItem)) {
+        this._$activeNavItem.removeClass("active");
+        this._$activeNavItem = $selectedNav;
+        this._$activeNavItem.addClass("active");
+        this._filterProjects(category);
+    }
 };
 
-var minImageWidth = 350;
-var aspectRatio = 9/16;
-var spacing = 15;
-var transitionDuration = 800;
-var $projects, $categories, $grid, $activeNavItem;
-var cols, rows, w, h;
-
-function init() {
-    $grid = $("#portfolio-grid");
-
-    $projects = [];
-    $categories = {}; 
-    $grid.find(".project").each(function (index, element) {
-        var $element = $(element);
-        $projects.push($element);
-        var categoryNames = $element.data("categories").split(",");
-        for (var i = 0; i < categoryNames.length; i += 1) {
-            var category = $.trim(categoryNames[i]).toUpperCase();
-            if (!$categories[category]) $categories[category] = [$element];
-            else $categories[category].push($element);
-        }
-    });
-
-    createGrid();
-
-    $activeNavItem = $("#portfolio-nav a.active");
-    $("#portfolio-nav a").on("click", onNavClick);
-}
-
-function createGrid() {
-    $grid.css("position", "relative");
-    var gridWidth = $grid.innerWidth();
-    cols = Math.floor((gridWidth + spacing) / (minImageWidth + spacing));
-    rows = Math.ceil($projects.length / cols);
-    w = (gridWidth - ((cols - 1) * spacing)) / cols;
-    h = w * aspectRatio;
-
-    $projects.forEach(function ($element, index) {
-        var pos = indexToXY(index, cols)
-        $element.css({
-            position: "absolute",
-            top: pos.y + "px",
-            left: pos.x + "px",
-            width: w + "px",
-            height: h + "px"
-        });
-    });
-    
-    $grid.css({
-        height: h * rows + spacing * (rows - 1) + "px"
-    });    
-}
-
-function onNavClick(e) {
-    e.preventDefault();
-    var $target = $(e.target);
-    if ($target.is($activeNavItem)) return;
-    if ($activeNavItem.length) $activeNavItem.removeClass("active");
-    $target.addClass("active");
-    $activeNavItem = $target;
-    filterProjects($target.data("category"));
-}
-
-function filterProjects(category) {
-    // Figure out the selected elements
-    var $selectedElements;
-    if (category === "ALL") $selectedElements = $projects;
-    else $selectedElements = $categories[category] || [];
+PortfolioFilter.prototype._filterProjects = function (category) {
+    var $selectedElements = this._getProjectsInCategory(category);
 
     // Animate the grid to the correct height to contain the rows
-    $grid.stop();
-    var curRows = Math.ceil($selectedElements.length / cols);
-    $grid.animate({
-        height: h * curRows + spacing * (curRows - 1) + "px"
-    }, transitionDuration);
+    this._animateGridHeight($selectedElements.length);
     
     // Loop through all projects
-    $projects.forEach(function ($element) {
+    this._$projects.forEach(function ($element) {
         // Stop all animations
         $element.stop();
         // If an element is not selected: drop z-index & animate opacity -> hide
@@ -88,7 +78,7 @@ function filterProjects(category) {
             $element.css("zIndex", -1);
             $element.animate({
                 opacity: 0
-            }, transitionDuration, "easeInOutCubic", function () {
+            }, this._transitionDuration, "easeInOutCubic", function () {
                 $element.hide();
             });
         }
@@ -96,31 +86,128 @@ function filterProjects(category) {
         else {
             $element.show();
             $element.css("zIndex", 0);
-            var newPos = indexToXY(selectedIndex, cols);
+            var newPos = this._indexToXY(selectedIndex);
             $element.animate({ 
                 opacity: 1,
                 top: newPos.y + "px",
                 left: newPos.x + "px"
-            }, transitionDuration, "easeInOutCubic");
+            }, this._transitionDuration, "easeInOutCubic");
         }
-    });
+    }.bind(this));
 }
 
-function set3dTranslate($element, x, y, z) {
-    $element.css({
-        '-webkit-transform' : 'translate3d(' + x + "," + y + "," + z + ')',
-        '-moz-transform'    : 'translate3d(' + x + "," + y + "," + z + ')',
-        '-ms-transform'     : 'translate3d(' + x + "," + y + "," + z + ')',
-        '-o-transform'      : 'translate3d(' + x + "," + y + "," + z + ')',
-        'transform'         : 'translate3d(' + x + "," + y + "," + z + ')'
-    });
+PortfolioFilter.prototype._animateGridHeight = function (numElements) {
+    this._$grid.stop();
+    var curRows = Math.ceil(numElements / this._cols);
+    this._$grid.animate({
+        height: this._imageHeight * curRows + 
+            this._gridSpacing * (curRows - 1) + "px"
+    }, this._transitionDuration);
+};
+
+PortfolioFilter.prototype._getProjectsInCategory = function (category) {
+    if (category === "all") {
+        return this._$projects;
+    } else {
+        return (this._$categories[category] || []);
+    }        
+};
+
+PortfolioFilter.prototype._cacheProjects = function () {
+    this._$projects = [];
+    this._$categories = {};
+    this._$grid.find(".project").each(function (index, element) {
+        var $element = $(element);
+        this._$projects.push($element);
+        var categoryNames = $element.data("categories").split(",");
+        for (var i = 0; i < categoryNames.length; i += 1) {
+            var category = $.trim(categoryNames[i]).toLowerCase();
+            if (!this._$categories[category]) {
+                this._$categories[category] = [$element];
+            } else {
+                this._$categories[category].push($element);
+            }
+        }
+    }.bind(this));
+};
+
+// PortfolioFilter.prototype._calculateGrid = function () {
+//     var gridWidth = this._$grid.innerWidth();
+//     this._cols = Math.floor((gridWidth + this._gridSpacing) / 
+//         (this._minImageWidth + this._gridSpacing));
+//     this._rows = Math.ceil(this._$projects.length / this._cols);
+//     this._imageWidth = (gridWidth - ((this._cols - 1) * this._gridSpacing)) / 
+//         this._cols;
+//     this._imageHeight = this._imageWidth * (1 / this._aspectRatio);
+// };
+
+PortfolioFilter.prototype._calculateGrid = function () {
+    var gridWidth = this._$grid.innerWidth();
+    for (var i = 0; i < this._breakpoints.length; i += 1) {
+        if (gridWidth <= this._breakpoints[i].width) {
+            this._cols = this._breakpoints[i].cols;
+            this._gridSpacing = this._breakpoints[i].spacing;
+            break;
+        }
+    }
+    this._rows = Math.ceil(this._$projects.length / this._cols);
+    this._imageWidth = (gridWidth - ((this._cols - 1) * this._gridSpacing)) / 
+        this._cols;
+    this._imageHeight = this._imageWidth * (1 / this._aspectRatio);
+};
+
+PortfolioFilter.prototype._createGrid = function () {
+    this._calculateGrid();
+
+    this._$grid.css("position", "relative");
+    this._$grid.css({
+        height: this._imageHeight * this._rows + 
+            this._gridSpacing * (this._rows - 1) + "px"
+    });    
+
+    this._$projects.forEach(function ($element, index) {
+        var pos = this._indexToXY(index);
+        $element.css({
+            position: "absolute",
+            top: pos.y + "px",
+            left: pos.x + "px",
+            width: this._imageWidth + "px",
+            height: this._imageHeight + "px"
+        });
+    }.bind(this));    
 }
 
-function indexToXY(index, cols) {
-    var r = Math.floor(index / cols);
-    var c = index % cols; 
+PortfolioFilter.prototype._onNavClick = function (e) {
+    e.preventDefault();
+    var $target = $(e.target);
+    if ($target.is(this._$activeNavItem)) return;
+    if (this._$activeNavItem.length) this._$activeNavItem.removeClass("active");
+    $target.addClass("active");
+    this._$activeNavItem = $target;
+    var category = $target.data("category").toLowerCase();
+
+    history.pushState({
+        url: "/work.html",
+        query: { category: category }
+    }, null, "/work.html?category=" + category);
+
+    this._filterProjects(category);
+}
+
+PortfolioFilter.prototype._onProjectClick = function (e) {
+    e.preventDefault();
+    var $target = $(e.currentTarget);
+    var projectName = $target.data("name");
+    var url = "/projects/" + projectName + ".html";
+    this._loader.loadPage(url, {}, true);
+}
+
+
+PortfolioFilter.prototype._indexToXY = function (index) {
+    var r = Math.floor(index / this._cols);
+    var c = index % this._cols; 
     return {
-        x: c * w + c * spacing,
-        y: r * h + r * spacing
+        x: c * this._imageWidth + c * this._gridSpacing,
+        y: r * this._imageHeight + r * this._gridSpacing
     };
 }
