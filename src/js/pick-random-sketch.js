2f36ea8e5c1e76820c75f30615d2e8e0d4f082fa
Michael Hadley <michaelwesthadley@gmail.com>
2016-06-11 13:16:24 -0500

    Create a random sketch generator - uses cookies to random w/o resampling

---
+++ src/js/pick-random-sketch.js
@@ -0,0 +1,65 @@
+var cookies = require("js-cookie");
+var utils = require("./utilities.js");
+
+var sketchConstructors = {
+    "halftone-flashlight": 
+        require("./interactive-logos/halftone-flashlight-word.js"),
+    "noisy-word":
+        require("./interactive-logos/noisy-word-sketch.js"),
+    "connect-points":
+        require("./interactive-logos/connect-points-sketch.js")
+};
+var numSketches = Object.keys(sketchConstructors).length;
+var cookieKey = "seen-sketch-names";
+
+/**
+ * Pick a random sketch that user hasn't seen yet. If the user has seen all the
+ * sketches, just pick a random one. This uses cookies to track what the user 
+ * has seen already.
+ * @return {Function} Constructor for a Sketch class
+ */
+module.exports = function pickRandomSketch() {
+    var seenSketchNames = cookies.getJSON(cookieKey) || [];
+    console.log(seenSketchNames)
+
+    // Find the names of the unseen sketches
+    var unseenSketchNames = findUnseenSketches(seenSketchNames);
+
+    // All sketches have been seen
+    if (unseenSketchNames.length === 0) {
+        // If we've got more then one sketch, then make sure to choose a random
+        // sketch excluding the most recently seen sketch
+        if (numSketches > 1) {
+            seenSketchNames = [seenSketchNames.pop()];
+            var unseenSketchNames = findUnseenSketches(seenSketchNames);
+        } 
+        // If we've only got one sketch, then we can't do much...
+        else {
+            seenSketchNames = [];
+            unseenSketchNames = Object.keys(sketchConstructors);            
+        }
+    };
+
+    var randSketchName = utils.randArrayElement(unseenSketchNames);
+    seenSketchNames.push(randSketchName);
+
+    // Store the generated sketch in a cookie. This creates a moving 7 day
+    // window - anytime the site is visited, the cookie is refreshed.
+    cookies.set(cookieKey, seenSketchNames, { expires: 7 });
+
+    console.log(randSketchName)
+    console.log(seenSketchNames)
+    console.log()
+
+    return sketchConstructors[randSketchName];
+};
+
+function findUnseenSketches(seenSketchNames) {
+    var unseenSketchNames = [];
+    for (var sketchName in sketchConstructors) {
+        if (seenSketchNames.indexOf(sketchName) === -1) {
+            unseenSketchNames.push(sketchName);
+        }
+    }
+    return unseenSketchNames;
+}
